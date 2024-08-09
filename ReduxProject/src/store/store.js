9be282c205547402f4c63466e802785574e511b3
eric9208 <eric@jane.app>
2024-08-09 00:25:44 -0700

    Redux Project

---
+++ ReduxProject/src/store/store.js
@@ -0,0 +1,23 @@
+import { createStore } from "redux";
+
+// Initial state
+const initialState = {
+  count: 0,
+};
+
+// Reducer function
+function counterReducer(state = initialState, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return { ...state, count: state.count + 1 };
+    case "DECREMENT":
+      return { ...state, count: state.count - 1 };
+    default:
+      return state;
+  }
+}
+
+// Create Redux store
+const store = createStore(counterReducer);
+
+export default store;
